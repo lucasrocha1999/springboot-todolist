8a530faaadfaeb13672b25db51a6b1d0c6fc6081
Lucas Lira <lucasrocha446@gmail.com>
2023-10-09 22:28:01 -0300

    chore: initial commit

---
+++ src/main/java/br/com/lucaslira/todolist/user/UserModel.java
@@ -0,0 +1,8 @@
+package br.com.lucaslira.todolist.user;
+
+public class UserModel {
+
+    public String username;
+    public String name;
+    public String password;
+}
---
+++ src/test/java/br/com/lucaslira/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucaslira.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
